d255209f313c9fa9c339781bedcad81fc96469d9
Homum Ahsan <hahsan95@gmail.com>
2018-05-01 15:17:35 -0500

    got the map working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("Hello from JavaScript");
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFoYW4iLCJhIjoiY2pnbzNvaWw2MDEzcDMzbXVvNHB4dWRvdCJ9.xURI-1BJa3s63MVDhvbaQg';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
