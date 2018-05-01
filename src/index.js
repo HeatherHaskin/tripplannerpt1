6f1e58176e4b1c1f5301a59f770ced37abbab171
Heather Allyse <heather.haskin@outlook.com>
2018-05-01 15:34:44 -0500

    heather couldn't load map

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaGFoYW4iLCJhIjoiY2pnbzNvaWw2MDEzcDMzbXVvNHB4d
 
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  center:  [-87.638873, 41.894728], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat( [-87.638873, 41.894728]).addTo(map);
+
+
